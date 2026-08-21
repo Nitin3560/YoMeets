38098e958884086104d153d9b84f230364592cab
Nitin singh Rathore <nxr3560@mavs.uta.edu>
2026-08-21 02:18:43 -0500

    feat: add observe protocol messages

--- packages/protocol/src/index.ts
+++ packages/protocol/src/index.ts
@@ -1,1 +1,23 @@
-export {};
+import type { PageObservation } from "@yomeets/browser-core";
+
+export type ProtocolError = {
+  code: string;
+  message: string;
+};
+
+export type ObservePageRequest = {
+  type: "OBSERVE_PAGE";
+  requestId: string;
+  tabId?: number;
+};
+
+export type ObservePageResult = {
+  type: "OBSERVE_PAGE_RESULT";
+  requestId: string;
+  observation?: PageObservation;
+  error?: ProtocolError;
+};
+
+export type ExtensionRequest = ObservePageRequest;
+
+export type ExtensionResponse = ObservePageResult;
